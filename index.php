d85ae96f45ee4ca91592df5be770841a3d4b05ff
tropotek <info@tropotek.com>
2016-05-16 12:18:43 +1000

    Started adding the mvc files

--- index.php
+++ index.php
@@ -4,17 +4,17 @@
  * @link http://www.tropotek.com/
  * @license Copyright 2007 Michael Mifsud
  */
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\HttpCache\HttpCache;
-use Symfony\Component\HttpKernel\HttpCache\Store;
 
 $appPath = __DIR__;
 include($appPath . '/vendor/autoload.php');
 
 $config = \Tk\Config::getInstance();
 
+var_dump($config);
+/**
 $kernel = new App\Kernel($config->getEventDispatcher(), $config->getControllerResolver());
 // Enable Cache
 $kernel = new HttpCache($kernel, new Store($config->getCachePath()));
 $kernel->handle($config->getRequest())->send();
 $kernel->terminate($config->getRequest(), new Response());
+*/
