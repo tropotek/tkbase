836f44dfd25c3f8ddfa8117fb8babb082097abf8
tropotek <info@tropotek.com>
2016-05-17 15:41:08 +1000

    -

---
+++ src/App/Controller/Index.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ *
+ * @date 16-05-2016
+ * @author Michael Mifsud <info@tropotek.com>
+ * @link http://www.tropotek.com/
+ * @license Copyright 2016 Michael Mifsud
+ */
+namespace App\Controller;
+
+/** */
+class Index
+{
+
+    
+    public function __construct()
+    {
+        
+    }
+    
+    public function doDefault()
+    {
+        vd('======================================INDEX');
+    }
+    
+}
