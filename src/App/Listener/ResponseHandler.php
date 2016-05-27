5a55b4d0466f2fb6b4f3e597c1cc43473d372480
tropotek <info@tropotek.com>
2016-05-27 15:38:59 +1000

    Created a routing lib and removed it from http lib

--- src/App/Listener/ResponseHandler.php
+++ src/App/Listener/ResponseHandler.php
@@ -50,11 +50,11 @@ class ResponseHandler implements SubscriberInterface
     /**
      * NOTE: if you want to modify the template using its API
      * you must add the listeners before this one its priority is set to -1000
-     * make sure your handlers have a priority > -1000
+     * make sure your handlers have a priority > -100 so this is run last
      * 
      * Convert controller return types to a request
-     * Once this event is fired it will stop propagation, so other events
-     * using this name must be run with a priority > 1000
+     * Once this event is fired and a response is set it will stop propagation, 
+     * so other events using this name must be run with a priority > -100
      * 
      * @param ControllerResultEvent $event
      */
@@ -91,7 +91,7 @@ class ResponseHandler implements SubscriberInterface
     public static function getSubscribedEvents()
     {
         return array(
-            KernelEvents::VIEW => array(array('domModify'), array('convertControllerResult', -1000)),
+            KernelEvents::VIEW => array(array('domModify', -80), array('convertControllerResult', -100)),
             KernelEvents::RESPONSE => 'onResponse'
         );
     }
