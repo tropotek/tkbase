9aac26796ebedef04f4de2c48a2225b88b675c0b
godar <info@tropotek.com>
2018-07-11 09:32:58 +1000

    Minor Code Updates - dev

--- src/config/sql/mysql/000000.sql
+++ src/config/sql/mysql/000000.sql
@@ -1,48 +1,12 @@
 
 
 
-DROP TABLE IF EXISTS `subscriber`;
-CREATE TABLE IF NOT EXISTS `subscriber` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(128) NOT NULL DEFAULT '',
-  `email` varchar(128) NOT NULL DEFAULT '',
-  `notes` TEXT,
-  `active` TINYINT(1) NOT NULL DEFAULT 1,
-  `del` TINYINT(1) NOT NULL DEFAULT 0,
-  `modified` DATETIME NOT NULL,
-  `created` DATETIME NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB;
-
-DROP TABLE IF EXISTS `event`;
-CREATE TABLE IF NOT EXISTS `event` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `title` varchar(128) NOT NULL DEFAULT '',
-  `date_start` DATETIME NOT NULL,
-  `date_end` DATETIME NOT NULL,
-  `rsvp` TEXT,
-  `description` TEXT,
-
-  `street` VARCHAR(128) NOT NULL DEFAULT '',
-  `city` VARCHAR(128) NOT NULL DEFAULT '',
-  `state` VARCHAR(128) NOT NULL DEFAULT '',
-  `postcode` VARCHAR(16) NOT NULL DEFAULT '',
-  `country` VARCHAR(128) NOT NULL DEFAULT '',
-  `address` TEXT,                                        -- The full google address: [no] [street], [city], [state], [country]
-  `map_lat` DECIMAL(15,12) NOT NULL DEFAULT 0,
-  `map_lng` DECIMAL(15,12) NOT NULL DEFAULT 0,
-  `map_zoom` DECIMAL(4, 2) NOT NULL DEFAULT 14,
-
-  `notes` TEXT,
-  `active` TINYINT(1) NOT NULL DEFAULT 1,
-  `del` TINYINT(1) NOT NULL DEFAULT 0,
-  `modified` DATETIME NOT NULL,
-  `created` DATETIME NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
 
 
 
-
+INSERT INTO `user` (`id`, `name`, `email`, `username`, `password`, `role`, `active`, `hash`, `modified`, `created`)
+VALUES
+  (NULL, 'Administrator', 'admin@example.com', 'admin', MD5('password'), 'admin', 1, MD5('1admin'), NOW() , NOW()),
+  (NULL, 'User 1', 'user@example.com', 'user1', MD5('password'), 'user', 1, MD5('2user1'), NOW() , NOW())
+;
 
