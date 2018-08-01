35d26bc4a76520b4d7a779ae27676d185a9d1e5f
mifsudm <mifsudm@unimelb.edu.au>
2018-08-02 08:28:57 +1000

    ~Auto: Commit

--- src/config/sql/mysql/000000.sql
+++ src/config/sql/mysql/000000.sql
@@ -14,6 +14,6 @@
 TRUNCATE `user`;
 INSERT INTO `user` (`role_id`, `name`, `email`, `username`, `password`, `hash`, `modified`, `created`)
 VALUES
-  (1, 'Administrator', 'admin@example.com', 'admin', MD5('password'), 'admin', MD5('1admin'), NOW() , NOW()),
-  (2, 'User', 'user@example.com', 'user1', MD5('password'), 'user', MD5('2user'), NOW() , NOW())
+  (1, 'Administrator', 'admin@example.com', 'admin', MD5('password'), MD5('1admin'), NOW() , NOW()),
+  (2, 'User', 'user@example.com', 'user1', MD5('password'), MD5('2user'), NOW() , NOW())
 ;
