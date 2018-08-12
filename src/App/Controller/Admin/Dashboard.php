236da5185b484ae54dd30ca9c82ae998199c1489
mifsudm <mifsudm@unimelb.edu.au>
2018-08-13 09:53:23 +1000

    ~Auto: Commit

--- src/App/Controller/Admin/Dashboard.php
+++ src/App/Controller/Admin/Dashboard.php
@@ -29,6 +29,26 @@ class Dashboard extends \Bs\Controller\AdminIface
         $this->getActionPanel()->setEnabled(false);
 
 
+        // The menu tester
+
+        $menu = \Tk\Ui\Menu\Menu::create('Navigation');
+
+        $menu->append(\Tk\Ui\Menu\Item::create('Item 1', \Tk\Uri::create('/item1.html'), 'fa fa-cogs'));
+        $menu->append(\Tk\Ui\Menu\Item::create('Item 2', \Tk\Uri::create('/item2.html'), 'fa fa-user'));
+
+        $sub1 = $menu->append(\Tk\Ui\Menu\Item::create('Item 3', \Tk\Uri::create('/item3.html'), 'fa fa-user-md'));
+        $sub1->append(\Tk\Ui\Menu\Item::create('Item 3.1', \Tk\Uri::create('/item3-1.html'), 'fa fa-list'));
+        $sub1->append(\Tk\Ui\Menu\Item::create('Item 3.2', \Tk\Uri::create('/item3-2.html'), 'fa fa-home'));
+        $sub1->append(\Tk\Ui\Menu\Item::create('Item 3.3', \Tk\Uri::create('/item3-3.html'), 'fa fa-globe'));
+
+        $menu->append(\Tk\Ui\Menu\Item::create('Item 4', \Tk\Uri::create('/item4.html'), 'fa fa-building'));
+        $menu->append(\Tk\Ui\Menu\Item::create('Item 5', \Tk\Uri::create('/item5.html'), 'fa fa-empire'));
+
+        // append and prepend
+
+        // Sub menus
+
+        vd($menu->__toString());
     }
 
     /**
